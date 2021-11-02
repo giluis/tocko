df12c69310cc3c3320fb85d9abb2a114d2b6e0e7
giluis <lupevibogil@gmail.com>
2021-11-02 18:50:21 +0000

    testing out piping

--- index.ts
+++ index.ts
@@ -1,38 +1,23 @@
-import TestReport from "./functions.ts";
 
 const buf = new Uint8Array(1024);
-let n = await Deno.stdin.read(buf);
 
-export let report: TestReport;
-if (n) {
-  let r: string;
-  if (n > 4) {
-    r = new TextDecoder().decode(buf.subarray(0, n));
-    let json;
-    try {
-      json = JSON.parse(r)
-      report = TestReport.fromJson(json);
-    }catch(e){
-    }
-  } else {
-    report = TestReport.empty();
-  }
-} else {
-  report = TestReport.empty();
-}
-
-export function suite(suiteName: string, runnable: () => void) {
-  report.suite(suiteName, runnable);
+export function suite(suiteName: string, runnable: () => void){
+    console.log(`------- ${suiteName} -------`)
+    runnable();
+    console.log(`---`)
 }
 
 export function test(testName: string, runnable: () => void) {
-  report.test(testName, runnable);
+    try {
+        runnable();
+        printTest(testName,false)
+    } catch(err){
+        printTest(testName,true,err.msg);
+    }
 }
 
-export * from "./formatter.ts";
-export * from "./assertions.ts";
+export function printTest(testName:string,isFailure:boolean,msg:string="Test Passed Successfully"):void{
+    console.log(`\t${ testName } => ${isFailure?"FAILED":"PASSED"}: ${msg}\n`);
+}
 
-window.addEventListener("unload", (e) => {
-  e.preventDefault();
-  Deno.stdout.writeSync(new TextEncoder().encode(JSON.stringify(report)));
-});
+export * from "./assertions.ts";
